5ce8f491b7a91e7439880a82005e9aa3896e5549
shoya-k <ss.shoya777@gmail.com>
2019-11-12 17:04:53 +0900

    incremennt and decriment create

---
+++ src/action/index.js
@@ -0,0 +1,14 @@
+//actionとは
+//JavaScriptのオブジェクトのこと
+
+export const INCREMENT = "INCREMENT"
+export const DECREMENT = "DECREMENT"
+
+export const increment = () =>({
+        type: INCREMENT
+})
+
+
+export const decrement = () => ({
+        type: DECREMENT
+})
